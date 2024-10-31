84c77c0b381e83bae4c6b953a0f0be5151cf665d
Saroj Kumar Sharma <90202990+sarojkumar2003@users.noreply.github.com>
2024-10-31 18:19:36 +0000

    2prog

--- Simple_java/eight.java
+++ Simple_java/eight.java
@@ -1,7 +1,17 @@
 package Simple_java;
 
+import java.util.Scanner;
+
 public class eight {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num  =sc.nextInt();
+        int sum = 0;
+        while (num>0) {
+            sum+= num%10;
+            num/=10;
+        }
+        System.out.println("The sum of the digits is: " + sum);
     }
 }
